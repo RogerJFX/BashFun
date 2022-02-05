9de26d68567860fc1ed51a6b3eba6472ce2d9951
Roger <roger@crazything.de>
2022-02-05 17:04:19 +0100

    Update Math.sh

--- Math.sh
+++ Math.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# don't break big numbers
+export BC_LINE_LENGTH=0
+
 function Math() {
 	
-	# floating points and big numbers.
 	function output() {
 		echo $1 | bc -l
 	}
 	
 	function sum() {
-		term=$1; shift
+		local term=$1; shift
 		for var in "$@" ; do
 			term="$term+$var"
 		done
@@ -20,23 +22,34 @@ function Math() {
 	}
 	
 	function factorial() {
-		term="1"
+		local term="1"
 		for ((i = 2; i <= $1; i++)) ; do
 			term="$term*$i"
 		done
 		echo $(output $term)
 	}
 	
-	# a bit slow then...
-	function fibonacci() {
-		num=$1
+	# a bit slow then... - but fun
+	function fibonacciRec() {
+		local num=$1
 		if [[ $num = 1 || $num = 2 ]] ; then
 			echo 1
 		else 
 			a=$(($num-1))
 			b=$(($num-2))
-			echo $(output "$(fibonacci $a) + $(fibonacci $b)")
+			echo $(output "$(fibonacciRec $a) + $(fibonacciRec $b)")
 		fi
+	}
+	
+	# faster :O
+	function fibonacci() {
+		local low=0
+		local high=1
+		for ((i = $1; i > 1; i--)) ; do
+			high=$(output "$high+$low")
+			low=$(output "$high-$low")
+		done
+		echo $(output $high)
 	}
 	
 	function notImplemented() {
@@ -57,6 +70,9 @@ function Math() {
 		;;
 		"fib")
 			fibonacci $1
+		;;
+		"fibFun")
+			fibonacciRec $1
 		;;
 		*)
 			notImplemented
