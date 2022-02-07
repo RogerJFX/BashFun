bc03cb61fa69ebe87e2abb52daaa3ca18552f746
Roger <roger@crazything.de>
2022-02-07 18:44:19 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+# Next to come: a testing framework. *lol* - for the bash. I think, I will do that stuff ...
+
 . ./Math.sh
 
+# Just some "object" to hold two values. Lazy echo.
+function coords() {
+	echo "$1 $2"
+}
+	
 echo $(Math sum 11.2 1.3 900.1)
 
 echo $(Math avg 1.1 2.2 3.3)
 
 echo $(Math fact 32)
 
-time echo $(Math fibFun 18)
+echo "This should take some time"
+time echo $(Math fibFun 14)
 
-time echo $(Math fib 3000)
+echo "This as well"
+time echo $(Math fib 1000)
+
+coords1=$(coords 50.111511 8.680506) # Frankfurt
+coords2=$(coords 49.45052 11.08048) # Nuremberg
+
+time echo $(Math geoDist $coords1 $coords2)
+
+echo $(Math atan2 -12 0)
