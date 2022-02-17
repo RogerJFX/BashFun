f4ec42fa34ab0c8e5e25ec34e77fa086ff3c2869
roger <roger@crazy-roger.de>
2022-02-17 18:23:06 +0100

    no comment, I simply would not dare writing what I think

--- java/Abs.java
+++ java/Abs.java
@@ -2,19 +2,21 @@ import static java.lang.System.out;
 
 public class Abs {
 	
-	private static final int minInteger = Integer.MIN_VALUE;
-	private static final int maxInteger = Integer.MAX_VALUE;
+	private static final int MIN_INTEGER = Integer.MIN_VALUE;
+	private static final int MAX_INTEGER = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.exit(1);
 		}
 		if ("minInteger".equals(args[0])) {
-			out.println(minInteger);
+			out.println(MIN_INTEGER);
 		} else if ("maxInteger".equals(args[0])) {
-			out.println(maxInteger);
+			out.println(MAX_INTEGER);
 		} else if ("absOfMinInteger".equals(args[0])) {
-			out.println(Math.abs(minInteger));
+			out.println(Math.abs(MIN_INTEGER));
+		} else {
+			throw new IllegalArgumentException("Wot u said?");
 		}
 	}
 	
